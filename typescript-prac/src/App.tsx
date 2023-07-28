524f8dc293060b5904769dd782367e78763b1e23
Jiwon Park <xoxojw.dev@gmail.com>
2023-07-28 19:13:09 +0900

    state, form 연습

--- typescript-prac/src/App.tsx
+++ typescript-prac/src/App.tsx
@@ -1,7 +1,19 @@
+import React, { useState } from "react";
+
 import Circle from "./Circle";
 import styled from "styled-components";
 
 const App = () => {
+  const [value, setValue] = useState('');
+  const onChange = (e: React.FormEvent<HTMLInputElement>) => {
+    const { currentTarget: { value }, } = e;
+    setValue(value);
+  };
+  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log("hello", value);
+  }
+
   return (
     <>
       <div>
@@ -11,6 +23,17 @@ const App = () => {
           <h1>Lorem Ipsum</h1>
           <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
         </StyledParagraph>
+      </div>
+      <div>
+        <StyledForm onSubmit={onSubmit}>
+          <input
+            value={value}
+            onChange={onChange}
+            type="text"
+            placeholder="username"
+          />
+          <button>Log in</button>
+        </StyledForm>
       </div>
     </>
   );
@@ -26,4 +49,9 @@ const StyledParagraph = styled.div`
     font-size: 24px;
     font-weight: 500;
   }
+`
+
+const StyledForm = styled.form`
+  margin: 20px;
+  padding: 20px;
 `
